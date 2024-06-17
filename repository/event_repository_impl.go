30062654bee12e276b833eb0ada4228208ba72c6
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2024-06-17 21:09:55 +0700

    Feat: Added CheckTicketAvailability and UpdateSoldTicket function
    - CheckTicketAvailability: enable checking the availability of tickets for a specified event and quantity.
    - UpdateSoldTicket: allow updating the sold ticket count for a specified event and quantity.

--- repository/event_repository_impl.go
+++ repository/event_repository_impl.go
@@ -73,3 +73,40 @@ func (repo *EventRepositoryImpl) DeleteEvent(Id string) (domain.Event, error) {
 
 	return eventData, nil
 }
+
+func (repo *EventRepositoryImpl) CheckTicketAvailability(eventId string, quantity int) (bool, error) {
+	var eventData domain.Event
+
+	err := repo.db.First(&eventData, "id = ?", eventId).Error
+
+	if err != nil {
+		return false, errors.New("event not found")
+	}
+
+	if eventData.Tickets < quantity {
+		return false, errors.New("ticket not available")
+	}
+
+	return true, nil
+}
+
+func (repo *EventRepositoryImpl) UpdateSoldTicket(eventId string, quantity int) (bool, error) {
+	var eventData domain.Event
+
+	err := repo.db.First(&eventData, "id = ?", eventId).Error
+
+	if err != nil {
+		return false, errors.New("event not found")
+	}
+
+	eventData.TicketsSold += quantity
+	eventData.Tickets -= quantity
+
+	err = repo.db.Save(&eventData).Error
+
+	if err != nil {
+		return false, errors.New("failed when update event")
+	}
+
+	return true, nil
+}
--- repository/event_repostitory.go
+++ repository/event_repostitory.go
@@ -8,4 +8,6 @@ type EventRepository interface {
 	GetListEvent() ([]domain.Event, error)
 	UpdateEvent(event domain.Event) (domain.Event, error)
 	DeleteEvent(Id string) (domain.Event, error)
+	CheckTicketAvailability(eventId string, quantity int) (bool, error)
+	UpdateSoldTicket(eventId string, quantity int) (bool, error)
 }
